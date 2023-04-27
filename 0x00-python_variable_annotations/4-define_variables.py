32288f6f3ac13cf7114325b3906ba5f45b1c8e17
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-27 22:55:11 +0300

    python_variable_annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" module 4-define_variables.py """
+
+
+a: int = 1
+""" An integer with a value of 1. """
+
+pi: float = 3.14
+""" A float with a value of 3.14. """
+
+i_understand_annotations: bool = True
+""" A boolean with a value of True. """
+
+school: str = "Holberton"
+""" A string with a value of "Holberton". """
