e81294698c92929fdebb2c15fdd9b2265f91c4c8
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-27 19:13:07 +0300

    python_variable_annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" module 0-add.py """
+
+
+def add(a: float, b: float) -> float:
+    """Finds the sum of two float numbers.
+    Args:
+        a: A float number.
+        b: A float number.
+    Returns:
+        The sum of a and b as a float number.
+    """
+    return a + b
