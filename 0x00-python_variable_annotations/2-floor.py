cb33170a7d3f5772e63e00fee4fdb68d6384e2cf
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-27 22:35:01 +0300

    python_variable_annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" module 2-floor.py """
+
+
+def floor(n: float) -> int:
+    """Return the floor of a given float number.
+    Args:
+        n: A float number.
+    Returns:
+        The largest integer less than or equal to n.
+    """
+    return int(n // 1)
