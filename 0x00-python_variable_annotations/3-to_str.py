894b89ebaba0b66dd458408793f04e02df983f3d
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-27 22:47:37 +0300

    python_variable_annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" module 3-to_str.py """
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a given float number.
+    Args:
+        n: A float number.
+    Returns:
+        The string representation of n.
+    """
+    return str(n)
