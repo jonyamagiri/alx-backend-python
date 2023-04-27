9bc062f8a62b87e97298d4f7512d2dc8add3db7b
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-04-27 19:22:13 +0300

    python_variable_annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" module 1-concat.py """
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings into a new string.
+    Args:
+        str1: A string to be concatenated.
+        str2: A string to be concatenated.
+    Returns:
+        A new string that is the concatenation of str1 and str2.
+
+    """
+    return str1 + str2
